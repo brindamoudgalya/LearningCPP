3a0e2ab0dbda1cab2cbf41c3aba4dd323da4e04a
Brinda Moudgalya <brindam@Brindas-MacBook-Air.local>
2025-11-09 11:54:27 -0800

    changes to userinput -- added function stuff

--- HELLO.cpp
+++ HELLO.cpp
@@ -17,17 +17,18 @@ void hello3() {
     std::cout << "Hello World 3.2!" << std::endl;
 }
 
+// commenting main out bc i reference HELLO.cpp from UserInput.cpp
 
-int main() {
-    std::cout << "Hello World 0!\n"; 
-    // << basically puts whatever comes after into the stream (output)
-    // std is a namespace and defines a scope for what comes after it
-    // :: says that the next thing is within the scope of the previous thing
+// int main() {
+//     std::cout << "Hello World 0!\n"; 
+//     // << basically puts whatever comes after into the stream (output)
+//     // std is a namespace and defines a scope for what comes after it
+//     // :: says that the next thing is within the scope of the previous thing
 
-    hello1();
-    std::cout << hello2() << std::endl;
-    hello3("Hello World 3.1!");
-    hello3();
+//     hello1();
+//     std::cout << hello2() << std::endl;
+//     hello3("Hello World 3.1!");
+//     hello3();
 
-    return 0;
-}
+//     return 0;
+// }
--- UserInput.cpp
+++ UserInput.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "HELLO.cpp" // including functions from a different file
 int withoutReference (int x); // function declaration (now main can call input() even if input() is defined below main in this file)
 int& withReference (int& x);
 
@@ -13,17 +14,22 @@ int main() {
     withoutReference(x);
     std::cout << "Without reference, the value of x doesn't change -- x = " << x << std::endl;
     withReference(x);
-    std::cout << "With reference, the value of x changes -- x = " << x << std::endl;
+    std::cout << "With reference, the value of x changes -- x = " << x << std::endl << std::endl << std::endl;
+
+    // calling methods from HELLO.cpp
+    hello1();
+    hello3("hey");
+    hello3();
 
     return 0;
 }
 
-int withoutReference (int x) { // modifies a copy of the value of x
+int withoutReference (int x) { // modifies a copy of the value of x ("passing by value")
     x++;
     return x;
 }
 
-int& withReference (int& x) { // modifies the actual value in the actual memory location of x
+int& withReference (int& x) { // modifies the actual value in the actual memory location of x ("passing by reference")
     x++;
     return x;
 }
